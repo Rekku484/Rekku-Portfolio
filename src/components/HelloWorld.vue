8fa0cfc4d076f927f969c70527d04458c63dba26
Rekku484 <137368315+Rekku484@users.noreply.github.com>
2024-06-30 15:27:09 +0800

    test 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,59 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+  <title>Rekku484</title>
+  <div id="landing">
+    <img src="/src/assets/logo.svg" width="250" height="250" />
+    <h1>WORKING IN PROGRESS...</h1>
+    <h2>STAY TUNE!</h2>
   </div>
+  <div id="plexus"></div>
 </template>
 
 <style scoped>
+#landing {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  flex-direction: column;
+}
+
+#landing {
+  min-height: 100%;
+  background: radial-gradient(circle, transparent, rgba(0, 0, 0, 1.5));
+  background-size: cover;
+}
+
+img {
+  filter: drop-shadow(#f0f8ff 0px 0px 30px);
+  transition:
+    transform 0.5s,
+    rotate 0.5s,
+    filter 0.5s;
+}
+
+img:hover {
+  transform: scale(1.1);
+  rotate: 1deg;
+  filter: drop-shadow(#f0f8ff 0px 0px 45px);
+}
+
+body {
+  height: 100vh;
+  background-color: #04232e;
+}
+
 h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
+  font-family: Arial, Helvetica, sans-serif;
+  color: #f0f8ff;
+  font-size: 35px;
   text-align: center;
+  text-shadow: #f0f8ff 1px 0px 25px;
 }
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+h2 {
+  font-family: Arial, Helvetica, sans-serif;
+  color: #f0f8ff;
+  font-size: 35px;
+  text-align: center;
+  text-shadow: #f0f8ff 1px 0px 25px;
 }
 </style>
