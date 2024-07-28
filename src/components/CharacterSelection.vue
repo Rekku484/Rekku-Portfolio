f1fc37a52fb21724cebeb52ee56f9038100b1b29
Rekku484 <137368315+Rekku484@users.noreply.github.com>
2024-07-28 13:02:06 +0800

    charactershowcase fix text overflowing

--- src/components/CharacterSelection.vue
+++ src/components/CharacterSelection.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="characters">
-    <div id="rafa">
-      <a>"RAFA"</a>
-      <div id="rafa_img"></div>
-      <b>CAT SPIRIT</b>
+  <div class="char">
+    <div class="showcase">
+      <div id="rafa">
+        <a>"RAFA"</a>
+        <div id="rafa_img"></div>
+        <b>CAT SPIRIT</b>
+      </div>
     </div>
-
-    <div id="rishi"></div>
+    <div class="showcase">
+      <div id="rishi"><a>"RISHI"</a><b>SLUGCAT</b></div>
+    </div>
   </div>
 </template>
 
@@ -17,30 +20,26 @@ export default {
 </script>
 
 <style scoped>
-.characters {
+.char {
   display: flex;
   justify-content: center;
 }
 
-a,
-b {
-  font-family: 'Times New Roman', Times, serif;
-  font-weight: bold;
-  font-size: 150px;
-  color: #62c0ff;
-  user-select: none;
-  white-space: nowrap;
+.showcase {
+  position: relative;
+  width: 80vh;
+  height: 50vh;
+  margin: 1%;
 }
 
 #rafa,
 #rishi {
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  margin: 1%;
+  flex-direction: column;
+  position: absolute;
   overflow: hidden;
-  width: 80vh;
-  height: 50vh;
+  width: 100%;
+  height: 100%;
   background-color: rgb(3, 11, 16);
   border-radius: 50px;
   transition: background-color 0.5s cubic-bezier(0.87, 0, 0.13, 1);
@@ -51,36 +50,37 @@ b {
   background-color: rgb(9, 40, 60);
 }
 
+a,
+b {
+  font-family: 'Times New Roman', Times, serif;
+  font-weight: bold;
+  font-size: 150px;
+  filter: blur(2px) brightness(0.5) grayscale();
+  color: #62c0ff;
+  user-select: none;
+  white-space: nowrap;
+}
+
 a {
   z-index: 3;
   position: absolute;
-  height: 50vh;
-  width: 80vh;
-  bottom: 0px;
+  bottom: -25px;
   left: -50px;
-  transition: left 0.7s cubic-bezier(0.87, 0, 0.13, 1);
+  transition:
+    left 0.7s cubic-bezier(0.87, 0, 0.13, 1),
+    filter 0.5s cubic-bezier(0.87, 0, 0.13, 1);
 }
 
-#rafa:hover a {
+#rafa:hover a,
+#rishi:hover a {
   left: 10px;
-}
-
-b {
-  z-index: 1;
-  height: 50vh;
-  width: 80vh;
-  left: 300px;
-  position: absolute;
-  transition: left 0.7s cubic-bezier(0.87, 0, 0.13, 1);
-}
-#rafa:hover b {
-  left: 150px;
+  filter: none;
 }
 
 #rafa_img {
   z-index: 2;
-  display: flex;
   position: relative;
+  filter: grayscale() brightness(0.5) blur(2px);
   overflow: hidden;
   width: 80vh;
   height: 50vh;
@@ -88,41 +88,28 @@ b {
   background-size: cover;
   background-repeat: no-repeat;
   background-position-x: -25px;
-  transition: background-position 0.5s cubic-bezier(0.87, 0, 0.13, 1);
+  transition:
+    background-position 0.5s cubic-bezier(0.87, 0, 0.13, 1),
+    filter 0.5s cubic-bezier(0.87, 0, 0.13, 1);
 }
 
 #rafa:hover #rafa_img {
-  background-position-x: -50px;
-}
-
-/* #rafa,
-#rishi {
-  display: flex;
-  flex-direction: column;
-  filter: grayscale() opacity(0.5);
-  overflow: hidden;
-  background-color: rgb(3, 11, 16);
-  border-radius: 50px;
-  margin: 1%;
-  width: 80vh;
-  height: 50vh;
-  transition:
-    background-color 0.5s cubic-bezier(0.87, 0, 0.13, 1),
-    filter 0.5s cubic-bezier(0.87, 0, 0.13, 1),
-    background-position-x 0.5s cubic-bezier(0.87, 0, 0.13, 1);
-}
-
-#rafa:hover,
-#rishi:hover {
   filter: none;
-  background-color: rgb(9, 40, 60);
   background-position-x: -50px;
 }
 
-#rafa {
-  background-image: url(/src/assets/characters/rafa.png);
-  background-position-x: -px;
-  background-size: cover;
-  background-repeat: no-repeat;
-} */
+b {
+  z-index: 1;
+  top: -25px;
+  left: 300px;
+  position: absolute;
+  transition:
+    left 0.7s cubic-bezier(0.87, 0, 0.13, 1),
+    filter 0.5s cubic-bezier(0.87, 0, 0.13, 1);
+}
+#rafa:hover b,
+#rishi:hover b {
+  filter: none;
+  left: 150px;
+}
 </style>
